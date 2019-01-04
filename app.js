7867fe2e57979150c4a14ebdf7957d69ec1eb7fd
StasCherednichenko <first1986st@gmail.com>
2019-01-04 09:59:13 +0200

    Added ejs templates and controllers to separate app for MVC

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error')
 
 const app = express();
 
-// Using handlebars
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
+// Using ejs
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+// Using handlebars
+// const expressHbs = require('express-handlebars');
+
+// app.engine('hbs',
+//     expressHbs(
+//         {
+//             extname: 'hbs',
+//             layoutsDir: 'views/layouts/',
+//             defaultLayout: 'main-layout',
+//         }
+//     )
+// );
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
 
 // Using pug
 // app.set('view engine', 'pug');
@@ -21,12 +36,10 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: '404 Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
 console.log('Listening localhost:3000');
