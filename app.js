893b40bf5b9724179ac290f17dd1833f33c78ac9
Stas Cherednichenko <first1986st@gmail.com>
2019-03-03 14:50:36 +0200

    Set cookies and sessions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -9,18 +11,37 @@ const User = require('./models/user');
 
 const app = express();
 
+const NONGO_DB_URI = 'mongodb+srv://stas:stas1986@cluster0-yk81v.mongodb.net/shop';
+const store = new MongoDBStore({
+    uri: NONGO_DB_URI,
+    collection: 'sessions'
+});
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
+    if (!req.session.user) {
+        return next();
+    };
+
     User
-        .findById('5c7783ee92851cbb42ae1385')
+        .findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
@@ -30,11 +51,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-    .connect('mongodb+srv://stas:stas1986@cluster0-yk81v.mongodb.net/shop?retryWrites=true')
+    .connect(NONGO_DB_URI)
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
